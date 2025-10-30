9f568e90756f88d31c4c4b875001eb1d5c3cd0ce
sacha-bouchez <sacha.bouchez@yahoo.com>
2025-10-30 16:54:45 +0000

    [data_loader.py] completed data loader

--- data/data_loader.py
+++ data/data_loader.py
@@ -2,7 +2,6 @@ import os
 
 import torch
 from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
 
 from phantom_simulation.object_simulator import Phantom2DPetGenerator
 from phantom_simulation.sinogram_simulator import SinogramSimulator
@@ -18,25 +17,32 @@ class SinogramGenerator(Dataset):
         self.image_size = image_size
         self.voxel_size = voxel_size
         self.phantom_generator = Phantom2DPetGenerator(shape=image_size, voxel_size=voxel_size)
-        self.sinogram_simulator = SinogramSimulator(binsimu=binsimu, save_castor=False)
+        self.sinogram_simulator = SinogramSimulator(binsimu=binsimu, save_castor=False, seed=seed)
         self.seed = seed
 
     def __len__(self):
         return self.length
+
+    def normalize(self, x):
+        return (x - torch.min(x)) / (torch.max(x) - torch.min(x))
 
     def generate_sample(self, idx):
 
         # Set seeds
         if self.seed is not None:
             torch.manual_seed(self.seed + idx)
+            torch.cuda.manual_seed_all(self.seed + idx)
             self.phantom_generator.set_seed(self.seed + idx)
-        
-        # Generate phantom
-        obj_path, att_path = self.phantom_generator.run(os.path.join(self.dest_path, f'data_{idx}', f'object'))
 
-        # Simulate sinogram
+        #
         dest_path = os.path.join(self.dest_path, f'data_{idx}')
-        self.sinogram_simulator.run(img_path=obj_path, img_att_path=att_path, dest_path=dest_path)
+        if not os.path.exists(f'{dest_path}/simu/simu_nfpt.s.hdr'):
+        
+            # Generate phantom
+            obj_path, att_path = self.phantom_generator.run(os.path.join(self.dest_path, f'data_{idx}', f'object'))
+
+            # Simulate sinogram
+            self.sinogram_simulator.run(img_path=obj_path, img_att_path=att_path, dest_path=dest_path)
 
         # Read hdr file to get matrix size
         with open(f'{dest_path}/simu/simu_nfpt.s.hdr', 'r') as f:
@@ -55,6 +61,11 @@ class SinogramGenerator(Dataset):
         data_noisy_1 = torch.poisson(data_nfpt)
         data_noisy_2 = torch.poisson(data_nfpt)
 
+        # Normalize
+        data_noisy_1 = self.normalize(data_noisy_1)
+        data_noisy_2 = self.normalize(data_noisy_2)
+        data_nfpt = self.normalize(data_nfpt)
+
         return data_noisy_1, data_noisy_2, data_nfpt
 
     def __getitem__(self, idx):
@@ -64,13 +75,25 @@ class SinogramGenerator(Dataset):
 
 if __name__ == '__main__':
 
+    from torch.utils.data import DataLoader
+    import matplotlib.pyplot as plt
+
     binsimu = os.path.join(os.getenv("WORKSPACE"), "simulator", "bin")
     dest_path = os.path.join(os.getenv("WORKSPACE"), "data")
-    dataset = SinogramGenerator(binsimu=binsimu, dest_path=dest_path, length=1, seed=42)
+    dataset = SinogramGenerator(binsimu=binsimu, dest_path=dest_path, length=2, seed=42)
     loader = DataLoader(dataset, batch_size=1, shuffle=False)
 
     for i, (noisy_1, noisy_2, clean) in enumerate(loader):
         print(f'Batch {i}:')
-        print(f'  Noisy 1 shape: {noisy_1.shape}, dtype: {noisy_1.dtype}')
-        print(f'  Noisy 2 shape: {noisy_2.shape}, dtype: {noisy_2.dtype}')
-        print(f'  Clean shape: {clean.shape}, dtype: {clean.dtype}')
+        print(f'  Noisy 1 avg: {torch.mean(noisy_1)},')
+        print(f'  Noisy 2 avg: {torch.mean(noisy_2)}')
+        print(f'  Clean avg: {torch.mean(clean)}')
+
+    fig, ax = plt.subplots(1,3, figsize=(12,4))
+    ax[0].imshow(noisy_1[0], cmap='gray')
+    ax[0].set_title('Noisy 1')
+    ax[1].imshow(noisy_2[0], cmap='gray')
+    ax[1].set_title('Noisy 2')
+    ax[2].imshow(clean[0], cmap='gray')
+    ax[2].set_title('Clean')
+    plt.show()
