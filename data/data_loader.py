ea36d4e02240250cdce96b14d40de55987a27105
sacha-bouchez <sacha.bouchez@yahoo.com>
2026-01-05 15:40:02 +0000

    [data_loader.py] adapted loader to new scaling conventions

--- data/data_loader.py
+++ data/data_loader.py
@@ -23,16 +23,31 @@ class SinogramGenerator(Dataset):
         self.length = length
         self.image_size = image_size
         self.voxel_size = voxel_size
+        #
         self.phantom_generator = Phantom2DPetGenerator(shape=image_size, voxel_size=voxel_size)
+        #
         if seed is None:
             self.seed = random.randint(0, 1e32)
         self.seed = seed
+        #
+        # Simulation parameters
+        self.nb_counts = 3e6
+        self.half_life = 109.8 * 60  # in seconds, F-18
+        self.acquisition_time = None
+        self.scatter_component = 0.35
+        self.random_component = 0.40
+        self.gaussian_PSF = 4  # in mm
+        #
         if simulator_type == 'castor':
             self.sinogram_simulator = SinogramSimulatorCastor(binsimu=binsimu, save_castor=False, seed=seed) # NOTE this seed is useless
         else:
             self.sinogram_simulator = SinogramSimulator(seed=seed)
+            if self.acquisition_time is None:
+                self.acquisition_time = self.sinogram_simulator.set_acquisition_time(n_samples=100, nb_counts=self.nb_counts, half_life=self.half_life, volume_activity=self.phantom_generator.volume_activity)
         #
         self.hashcode = self.get_generator_hashcode()
+
+        #
 
     def __len__(self):
         return self.length
@@ -61,7 +76,13 @@ class SinogramGenerator(Dataset):
             # Generate phantom
             obj_path, att_path = self.phantom_generator.run(os.path.join(self.dest_path, f'data_{data_hashcode}', f'object'))
             # Simulate sinogram
-            self.sinogram_simulator.run(img_path=obj_path, img_att_path=att_path, dest_path=dest_path)
+            self.sinogram_simulator.run(img_path=obj_path, img_att_path=att_path, dest_path=dest_path,
+                                        nb_counts=self.nb_counts,
+                                        half_life=self.half_life,
+                                        acquisition_time=self.acquisition_time,
+                                        scatter_component=self.scatter_component,
+                                        random_component=self.random_component,
+                                        gaussian_PSF=self.gaussian_PSF,)
 
         # Read hdr file to get matrix size
         data_nfpt = read_castor_binary_file(f'{dest_path}/simu/simu_nfpt.s.hdr').squeeze()
@@ -122,16 +143,20 @@ if __name__ == '__main__':
     from torch.utils.data import DataLoader
     import matplotlib.pyplot as plt
 
+    from tools.image.metrics import PSNR, SSIM
+
     binsimu = os.path.join(os.getenv("WORKSPACE"), "simulator", "bin")
     dest_path = os.path.join(os.getenv("WORKSPACE"), "data")
-    dataset = SinogramGenerator(binsimu=binsimu, dest_path=dest_path, length=2, seed=42)
+    dataset = SinogramGenerator('toy', binsimu=binsimu, dest_path=dest_path, length=2, seed=42)
     loader = DataLoader(dataset, batch_size=1, shuffle=False)
 
     for i, (noisy_1, noisy_2, clean) in enumerate(loader):
         print(f'Batch {i}:')
-        print(f'  Noisy 1 avg: {torch.mean(noisy_1)},')
-        print(f'  Noisy 2 avg: {torch.mean(noisy_2)}')
-        print(f'  Clean avg: {torch.mean(clean)}')
+        print(f'  Noisy 1 sum: {torch.sum(noisy_1)},')
+        print(f'  Noisy 1 PSNR: {SSIM(noisy_1.numpy(), clean.numpy()):.2f} dB')
+        print(f'  Noisy 2 sum: {torch.sum(noisy_2)}')
+        print(f'  Noisy 2 PSNR: {SSIM(noisy_2.numpy(), clean.numpy()):.2f} dB')
+        # print(f'  Clean avg: {torch.sum(clean)}')
 
     fig, ax = plt.subplots(1,3, figsize=(12,4))
     ax[0].imshow(noisy_1[0], cmap='gray')
